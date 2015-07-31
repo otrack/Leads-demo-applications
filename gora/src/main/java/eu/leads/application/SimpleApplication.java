fd7ae2cad53a93aa2c4da8988edd95ecc55f652f
otrack <0track@gmail.com>
2015-07-31 13:40:39 +0200

    [FIX] refine the demo to retrieve data through Gora

--- gora/src/main/java/eu/leads/application/SimpleApplication.java
+++ gora/src/main/java/eu/leads/application/SimpleApplication.java
@@ -1,5 +1,11 @@
 package eu.leads.application;
 
+import difflib.Delta;
+import difflib.DiffUtils;
+import difflib.Patch;
+import org.apache.gora.filter.FilterOp;
+import org.apache.gora.filter.SingleFieldValueFilter;
+import org.apache.gora.infinispan.query.InfinispanQuery;
 import org.apache.gora.query.Query;
 import org.apache.gora.query.Result;
 import org.apache.gora.store.DataStore;
@@ -7,6 +13,13 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.nutch.storage.StorageUtils;
 import org.apache.nutch.storage.WebPage;
 import org.apache.nutch.util.NutchConfiguration;
+
+import java.io.BufferedReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
@@ -20,94 +33,92 @@ public class SimpleApplication {
       Configuration configuration = NutchConfiguration.create();
       try {
 
-         // configuration
+         // 0 - Configuration
          DataStore<String,WebPage> store = StorageUtils.createStore(
                configuration, String.class, WebPage.class);
          store.createSchema();
 
+         // 1 - Print the total number of pages available in the store
+         // We use the API of Apache Gora and its support in ISPN.
          Query query = store.newQuery();
-         query.setFields("key","url");
-         query.setLimit(100);
-         Result<String,WebPage> result = query.execute();
-         int counter = 0;
-         while(result.next()){
-            System.out.println(".");
-            counter++;
-         }
-         System.out.println("Total amount of pages: "+counter);
+         query.setFields("key");
+         query.setLimit(1);
+         query.execute();
+         int total = ((InfinispanQuery)query).getResultSize();
+         System.out.println("Total amount of pages (in the store): "+total);
 
-//         // Retrieve the top 100 first pages from http://www.roadrunnersports.com/ (whatever the version is)
-//         // We use the API of Apache Gora and its support in ISPN.
-//         query = store.newQuery();
-//         query.setLimit(100);
-//         query.setFields("score", "url", "fetchTime", "content", "inlinks");
-//         query.setSortingOrder(false);
-//         query.setSortingField("score");
-//         SingleFieldValueFilter fieldValueFilter = new SingleFieldValueFilter();
-//         fieldValueFilter.setFieldName("url");
-//         fieldValueFilter.setFilterOp(FilterOp.LIKE);
-//         fieldValueFilter.setOperands(new String[] { "%roadrunnersports%" });
-//         query.setFilter(fieldValueFilter);
-//         result = query.execute();
-//         int averageInlinks = 0;
-//         int count = 0;
-//         Map<String,WebPage> resultMap = new HashMap<>();
-//         while(result.next()){
-//            WebPage page = result.get();
-//            // Print content and some usefull information
-//            System.out.println(
-//                  page.getUrl()
-//                        +", "+page.getScore()
-//                        +", "+page.getFetchTime()
-//                        +", "+(page.getContent()==null ? 0 : page.getContent().array().length));
-//            averageInlinks += page.getInlinks().size();
-//            count++;
-//
-//            if (!resultMap.containsKey(page.getUrl()) || resultMap.get(page.getUrl()).getFetchTime() < page.getFetchTime())
-//               resultMap.put(page.getUrl(),page);
-//         }
-//         System.out.println("Total amount of pages: "+count);
-//         System.out.println("Average in degree: "+(float)averageInlinks/(float)count);
-//
-//         // Compare the various versions of  roadrunnersports.com/rrs/womensshoes/womensnewshoes/
-//         query = store.newQuery();
-//         query.setSortingOrder(false);
-//         query.setSortingField("fetchTime");
-//         query.setLimit(100);
-//         fieldValueFilter = new SingleFieldValueFilter();
-//         fieldValueFilter.setFieldName("url");
-//         fieldValueFilter.setFilterOp(FilterOp.LIKE);
-//         fieldValueFilter.setOperands(new String[] { "%rgear-full-throttle-bottle-pack-21-ounce%" });
-//         query.setFilter(fieldValueFilter);
-//         result = query.execute();
-//
-//         List<String> previousText = null;
-//         WebPage previousPage = null;
-//         while(result.next()){
-//            WebPage currentPage = result.get();
-//            System.out.println(currentPage.getUrl()+", "+currentPage.getFetchTime());
-//            List<String> currentText = new ArrayList<>();
-//            BufferedReader reader =
-//                  new BufferedReader(
-//                        new StringReader(
-//                              new String(currentPage.getContent().array())));
-//            String line;
-//            while( (line=reader.readLine()) != null ){currentText.add(line);}
-//            if (previousText!=null) {
-//               System.out.println(
-//                     "****** version"
-//                           + previousPage.getFetchTime()
-//                           + " -- "
-//                           + currentPage.getFetchTime());
-//               Patch patch = DiffUtils.diff(previousText,currentText);
-//               for(Delta delta : patch.getDeltas()) {
-//                  System.out.println("+ "+delta.getOriginal().toString());
-//                  System.out.println("- " + delta.getRevised().toString());
-//               }
-//            }
-//            previousText = currentText;
-//            previousPage = currentPage;
-//         }
+         // 2 - Retrieve the top 100 first pages from http://www.roadrunnersports.com/ (whatever the version is)
+         query = store.newQuery();
+         query.setLimit(100);
+         query.setFields("score", "url", "fetchTime", "content", "inlinks");
+         query.setSortingOrder(false);
+         query.setSortingField("score");
+         SingleFieldValueFilter fieldValueFilter = new SingleFieldValueFilter();
+         fieldValueFilter.setFieldName("url");
+         fieldValueFilter.setFilterOp(FilterOp.LIKE);
+         fieldValueFilter.setOperands(new String[] { "%roadrunnersports%" });
+         query.setFilter(fieldValueFilter);
+         Result<String,WebPage> result = query.execute();
+         int averageInlinks = 0;
+         int count = 0;
+         Map<String,WebPage> resultMap = new HashMap<>();
+         while(result.next()){
+            WebPage page = result.get();
+            // Print content and some usefull information
+            System.out.println(
+                  page.getUrl()
+                        +", "+page.getScore()
+                        +", "+page.getFetchTime()
+                        +", "+(page.getContent()==null ? 0 : page.getContent().array().length));
+            averageInlinks += page.getInlinks().size();
+            count++;
+
+            if (!resultMap.containsKey(page.getUrl()) || resultMap.get(page.getUrl()).getFetchTime() < page.getFetchTime())
+               resultMap.put(page.getUrl(),page);
+         }
+         System.out.println("Total amount of pages: "+count);
+         System.out.println("Average in degree: "+(float)averageInlinks/(float)count);
+
+         // 3 - Compare the various versions of roadrunnersports.com/rrs/womensshoes/womensnewshoes/
+         query = store.newQuery();
+         query.setSortingOrder(false);
+         query.setSortingField("fetchTime");
+         query.setLimit(100);
+         fieldValueFilter = new SingleFieldValueFilter();
+         fieldValueFilter.setFieldName("url");
+         fieldValueFilter.setFilterOp(FilterOp.LIKE);
+         fieldValueFilter.setOperands(new String[] { "%roadrunnersports.com/rrs/womensshoes/womensnewshoes%" });
+         query.setFilter(fieldValueFilter);
+         result = query.execute();
+
+         List<String> previousText = null;
+         WebPage previousPage = null;
+         while(result.next()){
+            WebPage currentPage = result.get();
+            System.out.println(currentPage.getUrl()+", "+currentPage.getFetchTime());
+            List<String> currentText = new ArrayList<>();
+            if (currentPage.getContent()==null) continue;
+            BufferedReader reader =
+                  new BufferedReader(
+                        new StringReader(
+                              new String(currentPage.getContent().array())));
+            String line;
+            while( (line=reader.readLine()) != null ){currentText.add(line);}
+            if (previousText!=null) {
+               System.out.println(
+                     "****** version"
+                           + previousPage.getFetchTime()
+                           + " -- "
+                           + currentPage.getFetchTime());
+               Patch patch = DiffUtils.diff(previousText, currentText);
+               for(Delta delta : patch.getDeltas()) {
+                  System.out.println("+ "+delta.getOriginal().toString());
+                  System.out.println("- " + delta.getRevised().toString());
+               }
+            }
+            previousText = currentText;
+            previousPage = currentPage;
+         }
       } catch (Exception e) {
          e.printStackTrace();
       }
